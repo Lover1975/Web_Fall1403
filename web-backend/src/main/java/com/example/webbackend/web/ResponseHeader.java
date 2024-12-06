6038f736b879ee0852b4f90ac8ecdb83ce5a2dc8
Amirmohammad Eftekhar <amirmohammadeftekhar@gmail.com>
2024-12-06 20:55:24 +0330

    Remove server

--- web-backend/src/main/java/com/example/webbackend/web/ResponseHeader.java
+++
@@ -1,8 +0,0 @@
-package com.example.webbackend.web;
-
-public enum ResponseHeader {
-    OK, USERNAME_NOT_EXISTS, WRONG_PASSWORD, EMAIL_NOT_EXISTS, NOT_ALLOWED,
-    FOLLOWING_STATUS, REQUESTED_STATUS, FOLLOW_STATUS,
-    ROOM_EXISTS, ROOM_NOT_EXISTS
-    ;
-}
